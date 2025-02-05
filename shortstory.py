d8e5a18d09715fe08676720dc28d1a66fcfa99a3
aamabisca <aamabisca@mail.pima.edu>
2025-02-04 18:18:02 -0700

    Update shortstory.py
    Includes prints of a short story along with my name, title and description of what the code it for. There are five print statements with one of them having an addition \ to avoid further complications within the code.

--- shortstory.py
+++ shortstory.py
@@ -1,1 +1,8 @@
-print('Hello, World!')
+# Aurthur: Abigail Amabisca
+# My first Python lab in my CIS129 course
+# Prints a simple short story
+print('Within the woods is something that has never been seen before.')
+print('Cautiously, people approach.')
+print('The thing lifts it\'s head and stares at them.')
+print('It\'s a dragon!')
+print('The people scaddadle, running until they are out of breath.')
